4b19d952ca0719ed9811f57ab0a14c1a5e60abe9
Michael <michael.mcclung08@gmail.com>
2025-11-14 12:25:15 -0600

    Added in java code with spring boot website and maven

---
+++ rollwithdfw-backend/src/main/java/com/rollwithdfw/rollwithdfw_backend/RollwithdfwBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rollwithdfw.rollwithdfw_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollwithdfwBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollwithdfwBackendApplication.class, args);
+	}
+
+}
---
+++ rollwithdfw-backend/src/test/java/com/rollwithdfw/rollwithdfw_backend/RollwithdfwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rollwithdfw.rollwithdfw_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollwithdfwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
